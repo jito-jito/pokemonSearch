8b6cf4d2be9a0218d0337a016ab8c1fb146fb36c
sergio sanhueza <sergio@naitus.cl>
2023-01-19 21:48:30 -0300

    feat(config): initial config, add css bootstrap, popperjs and ng-bootstrap

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+	selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+	standalone: true,
+	imports: [NgbPaginationModule],
+})
+export class PaginationComponent {
+	page = 1;
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationComponent } from './components/pagination/pagination.component';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+  ]
+})
+export class SharedModule { }
